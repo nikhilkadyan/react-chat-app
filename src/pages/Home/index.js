e6331fa5dd24d89046d9ba2fecce055dc6c87227
Nikhil Kadyan <nikhilkadyan@Nikhils-MacBook-Pro.local>
2020-10-02 16:41:47 +0530

    Init base

---
+++ src/pages/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './style.css'
+const Home = () => {
+    return (
+        <div className="homeContainer">
+            <div>
+                <h1>React Group Chat</h1>
+                <p>
+                    By Nikhil Kadyan
+                    <button>Join Room</button>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default Home
